6a5ed3e00f519f1bfbcb5a9771efcdce0a9dbcda
wy <512458266@qq.com>
2022-05-05 20:44:33 +0800

    添加tag

--- static/js/tag.js
+++ static/js/tag.js
@@ -1,70 +1,100 @@
-$(function () {
-    var states = ['网络','Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-    ];
-    var tags = $('#input').tagsinput({
-        trimValue: true,
-        freeInput: true,
-        tagClass: function(item,a) {
-            return 'label label-primary';
-            // console.log(item)
-            // switch (item.continent) {
-            //     case 'Europe'   : return 'label label-primary';
-            //     case 'America'  : return 'label label-danger label-important';
-            //     case 'Australia':
-            //     case 'Africa'   : return 'label label-default';
-            //     case 'Asia'     : return 'label label-warning';
-            // }
-        },
-    });
-    var inputElm = $('#input').tagsinput('input');
+(function($){
+    $.fn.tags = function(option){
 
-    inputElm.attr("type",'search')
-    console.log(inputElm)
-    $('#input').on('itemAdded', function (event) {
-        hideTip()
-        $('#input').data("add",false)
-    });
-    $('#container').append("<div id='tip'></div>")
-    inputElm.on('input', function (val) {
-        var q = val.currentTarget.value;
-        var matches, substringRegex;
-        matches = [];
-        substrRegex = new RegExp(q, 'i');
-        var tipData = q ? states.filter(value => substrRegex.test(value)) : [];
-        showTip(tipData)
-    }).keydown(function(e) {
-        if(e.keyCode == 13){
-            $('#input').data("add",true)
-        }
-    }).blur(function () {
-        hideTip()
-    });
-    $('#input').on('beforeItemAdd', function(event) {
-        event.cancel = $('#input').data("add")?false:true
-    });
-    $("#tip").on('click', '.dataitem', function () {
-        var data = $(this).text();
-        $('#input').data("add",true)
-        $('#input').tagsinput('add', data);
-        $('#input').data("add",false)
-        hideTip()
-    })
+        var defaultSetting = { states:['网络','Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+                'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+                'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+                'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+                'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+                'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+                'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+                'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+                'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+            ],readonly:true }
+        var setting = $.extend(defaultSetting,option)
+        var {states,readonly} = setting
+        return this.each(function (index,e) {
+            $(this).tagsinput({
+                trimValue: true,
+                freeInput: true,
+                tagClass: function(item,a) {
+                    return 'label label-primary';
+                },
+            });
+            var $this = $(this)
+            var inputElm = $(this).tagsinput('input');
+            inputElm.attr("type",'search')
+            $(this).on('itemAdded', function (event) {
+                hideTip()
+                $this.data("add",false)
+            });
 
-    function showTip(data) {
-        var str = data.reduce((p, c) => p + `<div class="dataitem" tabindex="-1">${c}</div>`, "")
-        $('#tip').html(str).slideDown();
+            var bootstarpTag = inputElm.parent();
+
+            bootstarpTag.wrap('<div class="container">')
+            var container = bootstarpTag.parent();
+            container.append("<div class='tip'></div>")
+
+            if(readonly){
+                container.find('span[data-role="remove"]').hide();
+                inputElm.hide()
+                container.find('span.tag').css("paddingRight","6px")
+            }
+            var tip = container.find('.tip');
+            inputElm.on('input', function (val) {
+                var q = val.currentTarget.value;
+                var matches, substringRegex;
+                matches = [];
+                substrRegex = new RegExp(q, 'i');
+                var tipData = q ? states.filter(value => substrRegex.test(value)) : [];
+                showTip(tipData)
+            }).keydown(function(e) {
+                if(e.keyCode==40){
+                    tip.find('.dataitem').first().focus()
+                }
+                if(e.keyCode == 13){
+                    $this.data("add",true)
+                }
+            }).blur(function () {
+                if(container.find('.dataitem:focus').size()==0){
+                    hideTip()
+                }
+            });
+            $(this).on('beforeItemAdd', function(event) {
+                event.cancel = $this.data("add")?false:true
+            });
+            tip.on('click', '.dataitem', function () {
+                var data = $(this).text();
+                $this.data("add",true)
+                $this.tagsinput('add', data);
+                $this.data("add",false)
+                hideTip()
+            })
+
+            container.find('.tip').keydown(function(e) {
+                if(e.keyCode==40){
+                    container.find('.dataitem:focus').next(".dataitem").focus()
+                    //向上
+                }else if(e.keyCode==38){
+                    container.find('.dataitem:focus').prev(".dataitem").focus()
+                }
+            })
+
+
+
+            function showTip(data) {
+                var str = data.reduce((p, c,index) => p + `<div class="dataitem" tabindex="${index+1}">${c}</div>`, "")
+                tip.html(str).slideDown();
+            }
+
+            function hideTip() {
+                tip.slideUp()
+            }
+            return this;
+        })
+
     }
+})(jQuery);
 
-    function hideTip() {
-        $('#tip').slideUp()
-    }
 
-})
+
