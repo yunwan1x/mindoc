34c3a69cbbd03c464565756f900b17f8c8401892
wy <512458266@qq.com>
2022-05-22 10:54:30 +0800

    添加tag

--- controllers/LabelController.go
+++ controllers/LabelController.go
@@ -27,47 +27,6 @@ func (c *LabelController) Prepare() {
 	}
 }
 
-//查看包含标签的文档列表.
-func (c *LabelController) Indexold() {
-	c.Prepare()
-	c.TplName = "label/index.tpl"
-
-	labelName := c.Ctx.Input.Param(":key")
-	pageIndex, _ := c.GetInt("page", 1)
-	if labelName == "" {
-		c.Abort("404")
-	}
-	_, err := models.NewLabel().FindFirst("label_name", labelName)
-
-	if err != nil {
-		if err == orm.ErrNoRows {
-			c.Abort("404")
-		} else {
-			logs.Error(err)
-			c.Abort("500")
-		}
-	}
-	memberId := 0
-	if c.Member != nil {
-		memberId = c.Member.MemberId
-	}
-	searchResult, totalCount, err := models.NewBook().FindForLabelToPager(labelName, pageIndex, conf.PageSize, memberId)
-
-	if err != nil && err != orm.ErrNoRows {
-		logs.Error("查询标签时出错 ->", err)
-		c.ShowErrorPage(500, "查询文档列表时出错")
-	}
-	if totalCount > 0 {
-		pager := pagination.NewPagination(c.Ctx.Request, totalCount, conf.PageSize, c.BaseUrl())
-		c.Data["PageHtml"] = pager.HtmlPages()
-	} else {
-		c.Data["PageHtml"] = ""
-	}
-	c.Data["Lists"] = searchResult
-
-	c.Data["LabelName"] = labelName
-}
-
 func (c *LabelController) List() {
 	c.Prepare()
 	c.TplName = "label/list.tpl"
@@ -94,7 +53,8 @@ func (c *LabelController) List() {
 func (c *LabelController) Index() {
 	c.Prepare()
 	c.TplName = "search/label_index.tpl"
-
+	//重写controlName 标签卡不会 被选中
+	c.Data["ControllerName"] = ""
 	//如果没有开启你们访问则跳转到登录
 	if !c.EnableAnonymous && c.Member == nil {
 		c.Redirect(conf.URLFor("AccountController.Login"), 302)
